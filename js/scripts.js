0c0fa0074daebba0842c920f819e28621f1ed795
Sami Al-Jamal <sami.m.aljamal@gmail.com>
2016-06-17 10:18:27 -0700

    Add prototype to find the total price of all the toppings selected.

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,24 @@ function order(){
   this.pizzaSize = 0;
   this.price = 0;
 }
+order.prototype.toppingsPrice = function(toppings){
+  return this.toppings = 1.50 * toppings;
+
+}
 
 $(document).ready(function(){
+  var totalOrder;
   $("form#order").submit(function(event){
     event.preventDefault();
     //get toppings value
     var inputTopping = 0;
     $('input[type=checkbox]').each(function () {
     this.checked ? inputTopping += 1 : inputTopping += 0;
-  });ß
-    var inputSize = parseInt($("#size").val());
-
   });
-
+    alert(inputTopping);
+    var inputSize = parseInt($("#size").val());
+    var totalOrder= new order();
+    var toppingPrice = totalOrder.toppingsPrice(inputTopping);
+    alert(toppingPrice);
+  });
 });
