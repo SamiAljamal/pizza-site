f6489c1a18a539cedb059f58fd5b474733942091
Sami Al-Jamal <sami.m.aljamal@gmail.com>
2016-06-17 15:01:03 -0700

    Add modal to display results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,75 @@
 // Business logic
 function order(){
-  this.toppings = 0;
+  this.MeatToppings = 0;
+  this.veggieToppings = 0;
   this.pizzaSize = 0;
   this.price = 0;
 }
 //prototype to calculate the total price of toppings
-order.prototype.toppingsPrice = function(toppings){
-  return this.toppings = 1.50 * toppings;
+order.prototype.toppingsPriceMeat = function(toppings){
+  return this.meatToppings = 1 * toppings;
+}
+
+order.prototype.toppingsPriceVeggies = function(toppings){
+  return this.veggieToppings = .25 * toppings;
 }
 
 //prototype to calculate price according to the size of the pizza.
 order.prototype.sizePrice = function(pizzaSize){
   if(pizzaSize === 1){
-    this.pizzaSize = 8;
+    this.pizzaSize = 5;
   } else if (pizzaSize === 2) {
-    this.pizzaSize = 10;
-  } else {
-    this.pissaSize = 12;
+    this.pizzaSize = 7;
+  } else if (pizzaSize === 3){
+      this.pizzaSize = 9;
+  }
+   else{
+    this.pizzaSize = 11;
   }
   return this.pizzaSize;
 }
- order.prototype.totalPrice = function(toppings,size){
-   return this.price = toppings + size;
+ order.prototype.totalPrice = function(meatToppings,veggieToppings,size){
+   return this.price = meatToppings + veggieToppings + size;
  }
-
+// front-end logic
 $(document).ready(function(){
   var totalOrder;
   var toppingArray;
   $("form#order").submit(function(event){
     event.preventDefault();
-    //get toppings value
-    var inputTopping = 0;
-    var toppingArray=[];
+    //get topping values for veggies
+    var inputToppingVeggies= 0;
+    var toppingArrayVeggies = []
+    $('.chcbox1:checked').each(function(){
+      inputToppingVeggies += 1;
+    });
+    $('.chcbox1:checked').each(function () {
+      toppingArrayVeggies.push($(this).val());
+    });
+    //get toppings value for meat
+    var inputToppingMeat = 0;
+    var toppingArrayMeat=[];
     $('.chcbox:checked').each(function () {
-      inputTopping += 1;
-  });
-  alert(inputTopping);
-  $('.chcbox:checked').each(function () {
-    toppingArray.push($(this).val());
-  });
-  alert(toppingArray);
-
-
+      inputToppingMeat += 1;
+    });
+    $('.chcbox:checked').each(function () {
+      toppingArrayMeat.push($(this).val());
+    });
+    var toppingsArray=[]
+    toppingsArray.push(toppingArrayVeggies);
+    toppingsArray.push(toppingArrayMeat);
+    alert(toppingsArray);
     var inputSize = parseInt($("#size").val());
     var totalOrder= new order();
-    var toppingPrice = totalOrder.toppingsPrice(inputTopping);
-    alert(toppingPrice);
+    var toppingPriceMeat = totalOrder.toppingsPriceMeat(inputToppingMeat);
+    var toppingPriceVeggie = totalOrder.toppingsPriceVeggies(inputToppingVeggies);
     var pizzaSizePrice = totalOrder.sizePrice(inputSize);
-    alert(pizzaSizePrice);
-    var totalPrice = totalOrder.totalPrice(toppingPrice,pizzaSizePrice);
+    var totalPrice = totalOrder.totalPrice(toppingPriceMeat,toppingPriceVeggie,pizzaSizePrice);
     alert(totalPrice);
+    $(".toppings").text(toppingsArray);
+    $(".price").text(totalPrice);
+    $("#myModal").modal("show");
+
+
   });
 });
